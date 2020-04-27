aa426d109330df9a6ece569f6008e7f4f4a6c6b4
Robert Gordon <Komeuppance@gmail.com>
2020-04-27 10:35:25 -0700

    Added self __init__ for classes

--- app.py
+++ app.py
@@ -29,6 +29,19 @@ class User(db.Model):
     #use_item
     #examine
 
+    def __init__(self, username, password, portait, HP, MP, items, current_room, gold, move, attack, use_item, examine):
+        self.name = name
+        self.password = password
+        self.portait = portait
+        self.HP = HP
+        self.MP = MP
+        self.items = items
+        self.current_room = current_room
+        self.gold = gold
+        self.move = move
+        self.attack = attack
+        self.use_item = use_item
+        self.examine = examine
 
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -41,6 +54,16 @@ class Room(db.Model):
     # south
     # west
 
+    def __init__(self, title, description, items, NPCs, north, east, south, west):
+        self.title = title
+        self.description = description
+        self.items = items
+        self.NPCs = NPCs
+        self.north = north
+        self.east = east
+        self.south = south
+        self.west = west
+
 class NPC(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=True)
@@ -51,11 +74,28 @@ class NPC(db.Model):
     isHostile = db.Column(db.Boolean)
     attack = db.Column(db.Integer)
 
+    def __init__(self, name, description, items, gold, HP, isHostile, attack):
+        self.name = name
+        self.title = title
+        self.description = description
+        self.items = items
+        self.gold = gold
+        self.HP = HP
+        self.isHostile = isHostile
+        self.attack = attack
+
 class Merchant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
     #view inventory
     #buy from
     #sell to
+
+    def __init__(self, name, view_inventory, buy_from, sell_to):
+        self.name = name
+        self.view_inventory = view_inventory
+        self.buy_from = buy_from
+        self.sell_to = sell_to
 
 class Item:
     id = db.Column(db.Integer, primary_key=True)
@@ -64,6 +104,13 @@ class Item:
     price = db.Column(db.Integer)
     action = db.Column(db.String(1024))
     resuseable = db.Column(db.Boolean)
+
+    def __init__(self, title, description, price, action, reusable):
+        self.title = title
+        self.description = description
+        self.price = price
+        self.action = action
+        self.resuseable = reusable
 
 
 @app.route('/', methods=['GET'])
