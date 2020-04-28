2c459cdf6e9fd97af29545c57f56c8537d4cd451
Hui <jeffrey.x.hui@gmail.com>
2020-04-28 11:35:12 -0700

    merge in master

--- src/__init__.py
+++ src/__init__.py
@@ -17,18 +17,6 @@ db = SQLAlchemy()  # init orm for building tables/fields/constraints
 def create_app():
     app = Flask(__name__)
 
-<<<<<<< HEAD
-    # DB Configs
-    dburl = os.environ.get('DATABASE_URL')
-    app.config['SQLALCHEMY_DATABASE_URI'] = dburl
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-||||||| merged common ancestors
-    dburl = os.environ.get('DATABASE_URL')
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = dburl
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-=======
     dburl = os.environ.get("DATABASE_URL")
 
     app.config["SQLALCHEMY_DATABASE_URI"] = dburl
@@ -38,33 +26,20 @@ def create_app():
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
->>>>>>> eef7fb5152b7f31cff64c073ef7413fa9cfee57c
     migrate = Migrate(app, db)
     db.init_app(app)
 
-<<<<<<< HEAD
     # Route Registry
     from .public import public
     app.register_blueprint(public)
-||||||| merged common ancestors
-    from .public import public
 
-    app.register_blueprint(public)
-=======
+    # Auth Routes
     from .auth import auth
-
     app.register_blueprint(auth)
->>>>>>> eef7fb5152b7f31cff64c073ef7413fa9cfee57c
 
-<<<<<<< HEAD
     playerPrefix = '/player'
     # player Routes
     from .routes.movement import movement
     app.register_blueprint(movement, url_prefix=playerPrefix + '/movement')
 
     return app
-||||||| merged common ancestors
-    return app
-=======
-    return app
->>>>>>> eef7fb5152b7f31cff64c073ef7413fa9cfee57c
