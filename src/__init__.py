4eec993801cdb71a1e750ce682b77e5f0d0b769a
JimmyMcBride <mcbride967@gmail.com>
2020-04-28 10:15:05 -0500

    register route

--- src/__init__.py
+++ src/__init__.py
@@ -1,24 +1,31 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+from flask_login import LoginManager
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 import os
 
-db = SQLAlchemy() # init orm for building tables/fields/constraints
+db = SQLAlchemy()  # init orm for building tables/fields/constraints
+
 
 def create_app():
     app = Flask(__name__)
 
-    dburl = os.environ.get('DATABASE_URL')
+    dburl = os.environ.get("DATABASE_URL")
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = dburl
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = dburl
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
 
     migrate = Migrate(app, db)
 
--- src/public.py
+++ src/public.py
@@ -1,7 +1,54 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import Users
+from . import db
+import os
+import json
 
 public = Blueprint("public_routes", __name__)
 
-@public.route('/')
+secret = os.environ.get("DATABASE_URL")
+
+
+@public.route("/")
 def get_index():
     return "hello"
+
+
+@public.route("/login", methods=["POST"])
+def login():
+    return request
+
+
+@public.route("/registration/")
+def register():
+    return "hello"
+
+
+@public.route("/registration/", methods=["POST"])
+def register_request():
+    req = request.json
+
+    user = Users.query.filter_by(username=req["username"]).first()
+
+    # if a user is found, return an error
+    if user:
+        return "Username address already exists"
+
+    # create new user with the form data. Hash the password so plaintext version isn't saved.
+    if req["password1"] == req["password2"]:
+        new_user = Users(
+            username=req["username"],
+            password=generate_password_hash(req["password1"], method="sha256"),
+        )
+
+        # add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+        current_user = Users.query.filter_by(username=req["username"]).first()
+        print(current_user.username)
+        return f"Yay! Congratz, {current_user.username}! 🚀"
+
+    else:
+        return "Passwords don't match"
