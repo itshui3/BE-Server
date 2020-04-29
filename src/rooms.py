00397fc262629e9243f170e3f001b516dbb593ee
Robert Gordon <Komeuppance@gmail.com>
2020-04-29 09:01:49 -0700

    Floor 4 connected

--- src/rooms.py
+++ src/rooms.py
@@ -17,6 +17,7 @@ rooms.update(floor_4.make_floor(Room))
 floor_1.link_rooms(rooms)
 floor_2.link_rooms(rooms)
 floor_3.link_rooms(rooms)
+floor_4.link_rooms(rooms)
 
 #add items to rooms
 rooms['1-a2'].items = {(items['sword'])}
@@ -24,35 +25,36 @@ rooms['1-a2'].items = {(items['sword'])}
 # print(rooms['1-a1'].items)
 # print(f"Item here {items['sword']}")
 
+#This function checks all the rooms to ensure they are connected to the previous room
 def connect_backtrack(rooms):
     for key, current_room in rooms.items():
         if current_room.north: #if there is a connection to next room at north
             next_room = current_room.north #get next room from current
             rooms[next_room].south = current_room.title #connect next room's opposite direction to current room
-            print(f"\nCurrent room: {current_room.title} goes forwards to {current_room.north} At: north")
-            print(f"   Next room: {rooms[next_room].title} goes backward to {rooms[next_room].south} At: south")
+            # print(f"\nCurrent room: {current_room.title} goes forwards to {current_room.north} At: north")
+            # print(f"   Next room: {rooms[next_room].title} goes backward to {rooms[next_room].south} At: south")
 
         if current_room.east: #if there is a connection to next room at east
             next_room = current_room.east #get next room from current
             rooms[next_room].west = current_room.title #connect next room's opposite direction to current room
-            print(f"\nCurrent room: {current_room.title} goes forwards to {current_room.east} At: east")
-            print(f"   Next room: {rooms[next_room].title} goes backward to {rooms[next_room].west} At: west")
+            # print(f"\nCurrent room: {current_room.title} goes forwards to {current_room.east} At: east")
+            # print(f"   Next room: {rooms[next_room].title} goes backward to {rooms[next_room].west} At: west")
 
         if current_room.south: #if there is a connection to next room at south
             next_room = current_room.south #get next room from current
             rooms[next_room].north = current_room.title #connect next room's opposite direction to current room
-            print(f"\nCurrent room: {current_room.title} goes forwards to {current_room.south} At: south")
-            print(f"   Next room: {rooms[next_room].title} goes backward to {rooms[next_room].north} At: north")
+            # print(f"\nCurrent room: {current_room.title} goes forwards to {current_room.south} At: south")
+            # print(f"   Next room: {rooms[next_room].title} goes backward to {rooms[next_room].north} At: north")
 
         if current_room.west: #if there is a connection to next room at west
             next_room = current_room.west #get next room from current
             rooms[next_room].east = current_room.title #connect next room's opposite direction to current room
-            print(f"\nCurrent room: {current_room.title} goes forwards to {current_room.west} At: west")
-            print(f"   Next room: {rooms[next_room].title} goes backward to {rooms[next_room].east} At: east")
+            # print(f"\nCurrent room: {current_room.title} goes forwards to {current_room.west} At: west")
+            # print(f"   Next room: {rooms[next_room].title} goes backward to {rooms[next_room].east} At: east")
 
 connect_backtrack(rooms)
 
-#test print next room connection
+#This function prints all room connections
 def print_connections(rooms):
     for key, room in rooms.items():
         if room.north:
@@ -64,11 +66,6 @@ def print_connections(rooms):
         if room.west:
             print(f"Room: {room.title} Connects to: {room.west} At: west")
 
-print_connections(rooms)
+# print_connections(rooms)
 
 # print(rooms)
-
-# current = 0
-# for room in rooms.items():
-#     current += 1
-#     print(current, room)
