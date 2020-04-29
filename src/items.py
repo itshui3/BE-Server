8b02dee361794fbc17d445feb0037ee4c2afe89a
Robert Gordon <Komeuppance@gmail.com>
2020-04-28 19:27:10 -0700

    Added sword and potion

--- src/items.py
+++ src/items.py
@@ -1,3 +1,29 @@
 from models import Item
 
 items = {}
+
+sword = Item(
+    title = "Sword of Truth",
+    description = "Live or die by the truth",
+    price = 50,
+    action = "Slice",
+    damage = 50,
+    heal = 0,
+    armor = 0,
+    reusable = True
+)
+
+green_potion = Item(
+    title = "Green Potion",
+    description = "A delicious concotion",
+    price = 50,
+    action = "Power",
+    damage = 0,
+    heal = 50,
+    armor = 0,
+    reusable = True
+)
+
+items.update({'sword': sword})
+items.update({'green_potion': green_potion})
+print(items)
