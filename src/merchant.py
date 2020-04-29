c4d47ad5d2fa87977e5bf076b2f70336d7c98851
Robert Gordon <Komeuppance@gmail.com>
2020-04-29 12:58:35 -0700

    Update Merchant model

--- src/merchant.py
+++ src/merchant.py
@@ -1,5 +1,13 @@
-from models import Merchant
-from items import items
+from .models import Merchant
+from .items import items
 
-
-
+merchant_1 = Merchant(
+    name = 'Merchant 1',
+    inventory = {'leg_armor':items['leg_armor']},
+    gold = 1000,
+    hp = 1000,
+    attack = 1000,
+    view_inventory = "",
+    buy_from = "",
+    sell_to = ""
+)
