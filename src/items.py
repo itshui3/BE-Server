7af755a17e62841ed09c8b3e5dcdaac3bffbd66d
Robert Gordon <Komeuppance@gmail.com>
2020-04-29 09:58:44 -0700

    Adjust var name to items_blueprint

--- src/items.py
+++ src/items.py
@@ -124,10 +124,10 @@ items = {
 # print(items)
 
 
-items = Blueprint('items', __name__)
+items_blueprint = Blueprint('items', __name__)
 JWT_SECRET = os.environ.get("SECRET")
 
-@items.route('/', methods=['POST'])
+@items_blueprint.route('/', methods=['POST'])
 def the_stuff():
     userId = jwt.decode(request.headers['token'], JWT_SECRET)['user_id']
     user = db.session.query(Users).filter_by(id = userId)
