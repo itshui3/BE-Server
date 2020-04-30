219284808dcd7256b0620ce3e27293c419ecdb21
Robert Gordon <Komeuppance@gmail.com>
2020-04-30 13:21:25 -0700

    Starting pickin em up

--- src/items.py
+++ src/items.py
@@ -2,7 +2,8 @@ from flask import Blueprint, request
 import jwt
 import os
 from . import db
-from .models import Users, Room
+from .models import Users, Room, Item
+from .utils import parse_inventory, unparse_inventory, map_inventory
 
 items_blueprint = Blueprint('items', __name__)
 JWT_SECRET = os.environ.get("SECRET")
