77c9b6f138b2074754f30cff79ea79a84ddabfe3
Hui <jeffrey.x.hui@gmail.com>
2020-04-30 14:02:33 -0700

    added more monsters, added a monster catalogue spawn can refer to dependent on floor

---
+++ src/monster_catalogue.py
@@ -0,0 +1,7 @@
+# flattened string of monsters that can be spawned on a given fl by monster 'ref'
+monster_catalogue = {
+  "1": "goblin slime black_rat",
+  "2": "hobgoblin",
+  "3": "ogre bug_bear",
+  "4": "mind_flayer beholder lich"
+}
--- src/monsters_generator.py
+++ src/monsters_generator.py
@@ -35,5 +35,71 @@ monsters = {
         4, 
         True, 
         5
+    ),
+
+    "hobgoblin": Npc(
+        "Hobgoblin",
+        "hobgoblin",
+        "Larger, smarter cousins to goblins.",
+        '',
+        45,
+        40,
+        True,
+        10
+    )
+
+    "ogre": Npc(
+        "Ogre",
+        "ogre",
+        "Appearing as giant humanoids with obesely muscular bodies and large heads, these creatures stand between 9 to 10 feet tall and easily weigh more than 600 lbs.",
+        '',
+        300,
+        85,
+        True,
+        30
+    ),
+
+    "bug_bear": Npc(
+        "Bug Bear",
+        "bug_bear",
+        "Massive humanoid creatures distantly related to goblins and hobgoblins but larger and stronger.",
+        '',
+        350,
+        70,
+        True,
+        20
+    )
+
+    "mind_flayer": Npc(
+        'Mind Flayer',
+        'mind_flayer',
+        'Also known as Illithids, mind flayers are aberrants that dwell underground. They enslave races other races and their powerful psionic abilities make them a feared race.',
+        '',
+        0,
+        110,
+        True,
+        50
+    ),
+
+    "beholder": Npc(
+        'Beholder',
+        'beholder',
+        'Sometimes called a sphere of many eyes, or an eye tyrant. These large orb-shaped beings have ten eyestalks and one central eye, each containing powerful magic.',
+        '',
+        3000,
+        300,
+        True,
+        150
+    ),
+
+    "lich": Npc(
+        'Lich',
+        'lich',
+        'A type of undead creature. These powerful cadavers were once mortal wizards whose greed for eternal life and endless knowledge caused them to embrace undeath.',
+        '',
+        50000,
+        100,
+        True,
+        100
     )
 }
