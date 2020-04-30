82d80fc98a232a55fc974c6559d54ad56ffb976f
Robert Gordon <Komeuppance@gmail.com>
2020-04-29 21:49:30 -0700

    Parsing inventory string

--- src/merchant.py
+++ src/merchant.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, request
 import jwt
 import os
+import re
 from . import db
-from .models import Users, Room
+from .models import Users, Room, Merchant, Item
 
 merchant = Blueprint('merchant', __name__)
 JWT_SECRET = os.environ.get("SECRET")
@@ -10,10 +11,33 @@ JWT_SECRET = os.environ.get("SECRET")
 def make_a_merchant():
     userId = jwt.decode(request.headers['token'], JWT_SECRET)['user_id']
     user = db.session.query(Users).filter_by(id = userId).first()
+    merchant = db.session.query(Merchant).first()
+    items = db.session.query(Item).all()
     command = None
-    command = request.get_json()["direction"]
+    command = request.get_json()["command"]
+    buy_item = request.get_json()["buy_item"]
 
+    inventory = re.split(r'[-\s]', merchant.inventory)
 
-    print(f'user herrrrrrrre:{user.username}')
+    inv = {}
+    for i in range(0, len(inventory), 2):
+        item = inventory[i]
+        qty = int(inventory[i+1])
+        inv.update({item:qty})
 
-    return (user.current_room)
+    # print(f'user herrrrrrrre:{user.username}')
+    print(f'\nmerchant inventory: {inv}')
+
+    print(f'\nitems: {items}')
+    if command == 'peruse':
+        
+        peruse = {
+        }
+
+        return inv
+
+    elif command == 'buy':
+        print(f'buying: {buy_item}')
+        return "Buying item"
+    else:
+        return "Incorrect or unknown command"
