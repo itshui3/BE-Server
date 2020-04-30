46750e85cb0326c9d90e2f0c77457b5ef6506cf0
Robert Gordon <Komeuppance@gmail.com>
2020-04-29 17:58:29 -0700

    We've got movement...

--- src/movement.py
+++ src/movement.py
@@ -9,91 +9,108 @@ JWT_SECRET = os.environ.get("SECRET")
 @movement.route('/', methods=['POST'])
 def make_a_movement():
     userId = jwt.decode(request.headers['token'], JWT_SECRET)['user_id']
-    user = db.session.query(Users).filter_by(id = userId)
+    user = db.session.query(Users).filter_by(id = userId).first()
     command = None
     command = request.get_json()["direction"]
 
-    print(db.session.query(Room).filter_by(title = user['current_room']))
+    # print(f'user herrrrrrrre:{user.username}')
+    current_room = db.session.query(Room).filter_by(title = user.current_room).first()
     # print(current_room)
     rooms = db.session.query(Room).all()
 
-    print('\n\n', rooms, '\n\n')
+    # print('\n\n', rooms, '\n\n')
     try:
         command = request.get_json()['direction']
     except:
         print('failed to get direction from post req')
         return 'failed to get direction from post request'
     if command:
-        print(command)
+        print(f'\ncommand: {command}')
     else:
         print('no command')
         return 'command not set'
 
     # Directional Interface
     if command == 'n':
-        if current_room['north'] is None:
+        if current_room.north is None:
             return 'ya done goofed, no room here'
         else:
-            moveTo = current_room['north']
-            user['current_room'] = moveTo
+            moveTo = current_room.north
+            print(f'\nmoveTo: {moveTo}\n')
+            user.current_room = moveTo
+            db.session.commit()
+
+            room = user.current_room
 
             controls = {
                 user: user,
-                room: user['current_room'],
+                room: room,
                 #inventory ? inventory : null
                 #npc ? npc : null
                 #mob ? mob : null
             }
 
-            return controls
+            return f'moving to {room}'
 
     elif command == 'e':
-        if current_room['east'] is None:
+        if current_room.east is None:
             return 'ya done goofed, no room here'
         else:
-            moveTo = current_room['east']
-            user['current_room'] = moveTo
-            
+            moveTo = current_room.east
+            print(f'\nmoveTo: {moveTo}\n')
+            user.current_room = moveTo
+            db.session.commit()
+
+            room = user.current_room
+
             controls = {
                 user: user,
-                room: user['current_room'],
+                room: room,
                 #inventory ? inventory : null
                 #npc ? npc : null
                 #mob ? mob : null
             }
 
-            return controls
+            return f'moving to {room}'
 
     elif command == 's':
-        if current_room['south'] is None:
+        if current_room.south is None:
             return 'ya done goofed, no room here'
         else:
-            moveTo = current_room['south']
-            user['current_room'] = moveTo
-            
+            moveTo = current_room.south
+            print(f'\nmoveTo: {moveTo}\n')
+            user.current_room = moveTo
+            db.session.commit()
+
+            room = user.current_room
+
             controls = {
                 user: user,
-                room: user['current_room'],
+                room: room,
                 #inventory ? inventory : null
                 #npc ? npc : null
                 #mob ? mob : null
             }
 
-            return controls
+            return f'moving to {room}'
 
     elif command == 'w':
-        if current_room['west'] is None:
+        if current_room.west is None:
             return 'ya done goofed, no room here'
         else:
-            moveTo = current_room['west']
-            user['current_room'] = moveTo
-            
+            moveTo = current_room.west
+            print(f'\nmoveTo: {moveTo}\n')
+            user.current_room = moveTo
+            db.session.commit()
+
+            room = user.current_room
+
             controls = {
                 user: user,
-                room: user['current_room'],
+                room: room,
                 #inventory ? inventory : null
                 #npc ? npc : null
                 #mob ? mob : null
             }
 
-            return controls
+            return f'moving to {room}'
